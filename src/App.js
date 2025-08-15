8e01c57b5b95995b4b056920bcbc41a9777a3316
shahimshaar <shahmshaar5@gmail.com>
2025-08-15 17:31:40 -0500

    elis barbershop final showcase version

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
     email: 'hello@elisbarbershop.com',
     instagram: 'elis_barbershop_galesburg',
     bookingLink: 'https://booksy.com/en-us/1132493_elis-barbershop-of-galesburg_barber-shop_18668_galesburg?hl=en-US&gei=IYGfaK7kArqjptQPvo_i6Qg&rwg_token=ACgRB3cAikIcuGtwuba1VPgaXGAuE6P2vh6yvZ5NLM4adziAkfqt_ZwTDY8PH51SN_hpqEeZw6_p21y0EyL7Ox9dBJ7hLtX_5A%3D%3D#ba_s=seo',
-    mapsIframe: '"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3708.0743934335524!2d-90.36914912331837!3d40.94761247135914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87e1bff45c4f2b95%3A0x38d7ab8b683946df!2sEli%E2%80%99s%20Barbershop%20of%20Galesburg!5e1!3m2!1sen!2sus!4v1755296873188!5m2!1sen!2sus',    
+    mapsIframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3708.0743934335524!2d-90.36914912331837!3d40.94761247135914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87e1bff45c4f2b95%3A0x38d7ab8b683946df!2sEli%E2%80%99s%20Barbershop%20of%20Galesburg!5e1!3m2!1sen!2sus!4v1755296873188!5m2!1sen!2sus',    
     hours: "Tues: 12pm - 6pm | Wed thru Sat: 9am - 6pm | Sun & Mon: Closed"
   };
 
